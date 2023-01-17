e2002307fe4f13e57099c0808ba17a50426f6dfd
Richa77777 <121635433+Richa77777@users.noreply.github.com>
2023-01-17 23:18:56 +0300

    Add Change Scene Script
    Add Change Scene Script

---
+++ AnlautWinterJam2023_Project/Assets/Scenes/Minigames/Cute Invaders/Scripts/Fade.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CuteInvaders
+{
+    public class Fade : MonoBehaviour
+    {
+        private Animator _animator;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        public void Dark()
+        {
+            _animator.Play("FadeDark", -1, 0f);
+        }
+
+        public void Transparent()
+        {
+            _animator.Play("FadeTransparent", -1, 0f);
+        }
+
+        public void OffObject()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnObject()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
